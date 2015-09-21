e8a86ac5c0f0fab308f704dd678618378fa3aed8
Hamoud Abdulaziz <hamoud.aq@gmail.com>
2015-09-21 06:15:08 +0300

    Update Validation.py

--- Common/Validation.py
+++ Common/Validation.py
@@ -3,4 +3,4 @@ from flask_restful import reqparse
 
 #to get input from http header if data sent via post/put/delete
 UserInformation = reqparse.RequestParser()
-UserInformation.add_argument(("DNAME",type=str,help="User Displayed name")
+UserInformation.add_argument("DNAME",type=str,help="User Displayed name")
