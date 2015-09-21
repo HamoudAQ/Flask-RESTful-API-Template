e45ccdbc15ba1874be11d1a31c0a2459e1780a62
Hamoud Abdulaziz <hamoud.aq@gmail.com>
2015-09-21 06:08:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask,jsonify
 from Model import  db
+from flask.ext.restful import Api
+from resources.User import RU_User
 
 app = Flask(__name__)
 app.config.from_pyfile('Config.cfg')
 
 #Plug SQLAlchemy to your application
 db.init_app(app)
+
+#Plug restful to your application
+restful_api = Api(app)
 
 
 
@@ -18,3 +23,7 @@ def installer():
   db.create_all()
   return jsonify({"Success":"True","Message":"Application have been initialized successfully do not forget to make this secure or disable it after the installation"})
 
+
+
+#API Calls definition 
+restful_api.add_resource(RU_User,'/API/User')#GET for Read,PUT for update
