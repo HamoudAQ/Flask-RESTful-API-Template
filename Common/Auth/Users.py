1c07363119b7fe75797c369ff7ee6944246c1bc2
Hamoud Abdulaziz <hamoud.aq@gmail.com>
2015-11-29 14:30:17 +0300

    Update Users.py

--- Common/Auth/Users.py
+++ Common/Auth/Users.py
@@ -1,13 +1,13 @@
 from flask.ext.httpauth import HTTPBasicAuth
 from flask import  g
-from Model import Client
+from Model import Users
 
 auth = HTTPBasicAuth()
 
 
 @auth.verify_password
 def verify_password(username, password):#we consider User phone as username 
-    user = Client.query.filter_by(Phone = username).first()
+    user = Users.query.filter_by(Phone = username).first()
     if not user or not user.verify_password(password):
         return False
     g.user = user
